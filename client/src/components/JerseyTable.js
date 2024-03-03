216561cc9299647c4c3fbbd49d5585637f82b971
TXTEck <91459117+TXTEck@users.noreply.github.com>
2024-03-04 07:48:23 +0800

    finale ver

--- client/src/components/JerseyTable.js
+++ client/src/components/JerseyTable.js
@@ -125,7 +125,6 @@ export default class JerseyTable extends Component {
                             <th onClick={() => this.handleSort('size')}>Size {this.state.sortBy === 'size' && (this.state.sortAsc ? '▲' : '▼')}</th>
                             <th onClick={() => this.handleSort('colour')}>Colour {this.state.sortBy === 'colour' && (this.state.sortAsc ? '▲' : '▼')}</th>
                             <th onClick={() => this.handleSort('price')}>Price {this.state.sortBy === 'price' && (this.state.sortAsc ? '▲' : '▼')}</th>
-                            <th>    </th>
                         </tr>
                     </thead>
 
